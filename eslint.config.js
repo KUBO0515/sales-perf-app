bf3f5fec6c7bebaccdc65dde2dee076ee7e0ecc9
Uta-0077 <create.lab.ky0613@gmail.com>
2025-07-02 13:03:36 +0900

    Co-authored-by: htpvdev <htpvdev@users.noreply.github.com> Co-authored-by: nt-sasakure <NishidaTakuma@users.noreply.github.com>

---
+++ eslint.config.js
@@ -0,0 +1,58 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactPlugin from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import prettierPlugin from 'eslint-plugin-prettier'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat({
+  // eslint-disable-next-line no-undef
+  baseDirectory: import.meta.dirname ?? process.cwd(),
+})
+
+export default [
+  { ignores: ['dist'] },
+
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  ...compat.extends('plugin:react/recommended'),
+  ...compat.extends('plugin:prettier/recommended'),
+
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: globals.browser,
+    },
+
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier: prettierPlugin,
+    },
+
+    rules: {
+      ...reactHooks.configs.recommended.rules, // React Hooksの推奨ルール
+
+      'react/react-in-jsx-scope': 'off', // React18 以降は JSX に React import 不要のため無効化
+
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': 'error',
+    },
+
+    settings: {
+      react: {
+        version: 'detect', // Reactのバージョンを自動検出
+      },
+    },
+  },
+]
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
-
-import { defineConfig } from 'vite'
+import { defineConfig, configDefaults } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import pkg from './package.json'
 import tailwindcss from '@tailwindcss/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
 
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-  server: {
-    port: 5173
-  }
+  plugins: [react(),tsconfigPaths(),tailwindcss(),],
+    test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/test/setup.ts',
+    include: ['src/test/*.{test,spec}.{js,jsx,ts,tsx}'],
+    exclude: [...configDefaults.exclude],
+    coverage: { provider: 'v8', reporter: ['text', 'html'] },
+  },
+  define: {
+    'import.meta.env.APP_VERSION': JSON.stringify(pkg.version),
+  },
 })
