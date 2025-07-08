c7a4a51761d9b75f55f7ba10dd581d5c1d683945
Uta-0077 <create.lab.ky0613@gmail.com>
2025-07-08 17:32:48 +0900

    フォーマット作成とフォーマット一覧ページの作成＋遷移

---
+++ src/pages/admin/formats/CreateForm.tsx
@@ -0,0 +1,13 @@
+import AdminSidebar from '@components/AdminSidebar'
+import React from 'react'
+
+export default function CreateForm() {
+  return (
+    <>
+      <AdminSidebar />
+      <div className="ml-[280px] min-h-screen space-y-6 bg-gray-200 p-4">
+        CreateForm
+      </div>
+    </>
+  )
+}
