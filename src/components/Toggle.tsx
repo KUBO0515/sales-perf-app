412430b142a3803847bc92b67e3b1010e62a23d4
htpvdev <haruki20211121@gmail.com>
2025-07-11 18:34:09 +0900

    データ分析機能実装その1

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -26,21 +26,23 @@ export default function Toggle({
 
   return (
     <div className="flex items-center space-x-2">
-      <span className="">{label}</span>
+      <span className={checked ? 'text-gray-700' : 'font-bold'}>{label}</span>
       <button
         type="button"
         role="switch"
         aria-checked={checked}
         aria-label={label ?? 'toggle'}
         onClick={handleToggle}
-        className={`relative inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full transition-colors focus:ring-2 focus:ring-blue-500 focus:outline-none ${checked ? 'bg-blue-600' : 'bg-gray-300'}`}
+        className={`relative inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full bg-gray-400 transition-colors focus:ring-2 focus:ring-blue-500 focus:outline-none`}
       >
         {/* ツマミ */}
         <span
           className={`inline-block h-5 w-5 transform rounded-full bg-white shadow transition ${checked ? 'translate-x-5' : 'translate-x-1'}`}
         />
       </button>
-      <span className="ml-2 text-sm text-gray-700">{opponentLabel}</span>
+      <span className={`ml-2 ${checked ? 'font-bold' : 'text-gray-700'}`}>
+        {opponentLabel}
+      </span>
     </div>
   )
 }
--- src/pages/admin/analytics/index.tsx
+++ src/pages/admin/analytics/index.tsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from 'react'
 import { motion } from 'framer-motion'
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs, orderBy, query, where } from 'firebase/firestore'
 import { db } from '@/firebase'
 
 import ChartCard from '@components/ChartCard'
@@ -15,13 +15,25 @@ import {
   WeeklyAggregate,
   weeklyAggregateConverter,
 } from '@/types/WeeklyAggregate'
+import { ReportFormat, reportFormatConverter } from '@/types/ReportFormat'
+import {
+  ReportFormatInput,
+  reportFormatInputConverter,
+} from '@/types/ReportFormatInput'
 
 export default function Page() {
   const { appContext } = useContext(AppContext)
   const companyId = appContext.company.id ?? ''
 
-  const [isMonthly, setIsMonthly] = useState(true)
-  // const [targetInputId, setTargetInputId] = useState<string | null>(null)
+  const [isMonthly, setIsMonthly] = useState(false)
+
+  const [reportFormats, setReportFormats] = useState<ReportFormat[]>([])
+  const [currentReportFormatInputs, setCurrentReportFormatInputs] = useState<
+    ReportFormatInput[]
+  >([])
+
+  const [targetFormatId, setTargetFormatId] = useState('')
+  const [targetInputId, setTargetInputId] = useState<string | null>(null)
 
   const [aggregates, setAggregates] = useState<
     (MonthlyAggregate | WeeklyAggregate)[]
@@ -31,31 +43,70 @@ export default function Page() {
 
   const labels = aggregates.map((aggregate) => aggregate.timeLabel)
   const values = aggregates.map((aggregate) =>
-    Number(aggregate.reports['7NMexcpCrSh7iPX6WQIR'])
+    Number(aggregate.reports[targetInputId ?? 'value'] ?? 0)
   )
 
   const fetch = async () => {
+    // 報告フォーマットを全件取得
+    const newReportFormats = (
+      await getDocs(
+        collection(db, `companies/${companyId}/reportFormats`).withConverter(
+          reportFormatConverter
+        )
+      )
+    ).docs.map((doc) => doc.data())
+
+    const newTargetFormatId =
+      targetFormatId === '' ? newReportFormats[0]?.id : targetFormatId
+
+    // 対象の報告フォーマットの入力項目を取得
+    const newCurrentReportFormatInputs = (
+      await getDocs(
+        collection(
+          db,
+          `companies/${companyId}/reportFormats/${newTargetFormatId}/reportFormatInputs`
+        ).withConverter(reportFormatInputConverter)
+      )
+    ).docs
+      .map((doc) => doc.data())
+      .filter((input) => input.type === 'number')
+
     let aggregateSnapshot
     if (isMonthly) {
       aggregateSnapshot = await getDocs(
-        collection(
-          db,
-          `companies/${companyId}/monthlyAggregates`
-        ).withConverter(monthlyAggregateConverter)
+        query(
+          collection(
+            db,
+            `companies/${companyId}/monthlyAggregates`
+          ).withConverter(monthlyAggregateConverter),
+          where('reportFormatId', '==', newTargetFormatId),
+          orderBy('ym')
+        )
       )
     } else {
       aggregateSnapshot = await getDocs(
-        collection(db, `companies/${companyId}/weeklyAggregates`).withConverter(
-          weeklyAggregateConverter
+        query(
+          collection(
+            db,
+            `companies/${companyId}/weeklyAggregates`
+          ).withConverter(weeklyAggregateConverter),
+          where('reportFormatId', '==', newTargetFormatId),
+          orderBy('yearWeek')
         )
       )
     }
+
+    setReportFormats(newReportFormats)
+    setCurrentReportFormatInputs(newCurrentReportFormatInputs)
+    setTargetFormatId(newTargetFormatId || '')
+    setTargetInputId(newCurrentReportFormatInputs[0]?.id ?? null)
     setAggregates(aggregateSnapshot.docs.map((doc) => doc.data()))
   }
 
   useEffect(() => {
     fetch()
-  }, [isMonthly])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isMonthly, targetFormatId, targetInputId])
 
   return (
     <>
@@ -66,13 +117,58 @@ export default function Page() {
         transition={{ duration: 0.8, ease: 'easeOut' }}
         className="ml-[280px] min-h-screen space-y-6 bg-gray-200 p-4"
       >
-        <h1 className="text-2xl font-bold">Analytics</h1>
-        <Toggle
-          onChange={() => setIsMonthly((v) => !v)}
-          defaultChecked={isMonthly}
-          label="週間"
-          opponentLabel="月間"
-        />
+        <h1 className="text-2xl font-bold">データ分析</h1>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, ease: 'easeOut' }}
+          className="rounded-2xl bg-white p-4 shadow"
+        >
+          <div className="flex">
+            <Toggle
+              onChange={() => setIsMonthly((v) => !v)}
+              defaultChecked={isMonthly}
+              label="週間"
+              opponentLabel="月間"
+            />
+            <div className="ms-4 flex items-center border-s ps-4">
+              <label>報告フォーマット</label>
+              <select
+                className="ml-4 rounded border p-2"
+                onChange={(e) => setTargetFormatId(e.target.value)}
+              >
+                {reportFormats.map((format) => (
+                  <option
+                    key={format.id}
+                    value={format.id}
+                    selected={targetFormatId === format.id}
+                    onClick={() => setTargetFormatId(format.id || '')}
+                  >
+                    {format.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="ms-4 flex items-center border-s ps-4">
+              <label>項目</label>
+              <select
+                className="ml-4 rounded border p-2"
+                onChange={(e) => setTargetInputId(e.target.value)}
+              >
+                {currentReportFormatInputs.map((input) => (
+                  <option
+                    key={input.id}
+                    value={input.id}
+                    selected={targetInputId === input.id}
+                    onClick={() => setTargetInputId(input.id || null)}
+                  >
+                    {input.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+        </motion.div>
         <ChartCard {...{ title, labels, values }} />
       </motion.div>
     </>
